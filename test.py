6468d70c597ebce646db9993c08c25ab811365ce
Shashwat Singh <shashwats17@gmail.com>
2024-06-30 15:33:11 -0400

    THIS

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
-import calc
-
-def main():
-    test_square()
+from calc import square
 
 def test_square():
     try:
-        assert calc.square(2) == 4
-    except AssertionError:
-        print("2 squared wasnt 4")
-    try:
-        assert calc.square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")
+        assert square(2) == 4
+        assert square(-2) == 4
+        assert square(3) == 9
+        assert square(-3) == 9
+        assert square(0) == 0
+    except:
+        if square() == str:
+            print("wrong")
 
-if __name__ == "__main__":
-    main()
