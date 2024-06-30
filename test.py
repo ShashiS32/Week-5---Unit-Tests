ea379c9351f48e032744a640a566cd290da0eda6
Shashwat Singh <shashwats17@gmail.com>
2024-06-30 12:51:40 -0400

    assertion

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ def main():
     test_square()
 
 def test_square():
-    if calc.square(2) != 4:
-        print("2 squared was not 4")
-    if calc.square(3) != 9:
+    try:
+        assert calc.square(2) == 4
+    except AssertionError:
+        print("2 squared wasnt 4")
+    try:
+        assert calc.square(3) == 9
+    except AssertionError:
         print("3 squared was not 9")
-    
 
 if __name__ == "__main__":
     main()
