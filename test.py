b689c061aaa66d91d71db5453b7f6b8432e1cc11
Shashwat Singh <shashwats17@gmail.com>
2024-07-01 17:06:34 -0400

    skib

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 from calc import square
+import pytest
 
-def test_square():
-    try:
-        assert square(2) == 4
-        assert square(-2) == 4
-        assert square(3) == 9
-        assert square(-3) == 9
-        assert square(0) == 0
-    except:
-        if square() == str:
-            print("wrong")
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
+def test_negative():
+
+    assert square(-3) == 9
+
+    assert square(-2) == 4
+def test_zero():
+    assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square(str)
